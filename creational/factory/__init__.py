c399f29a71bdc9147b2a432f8b29d2ad3b16fa6e
cosmicoppai <kanakchaudhari12@gmail.com>
2022-11-28 02:50:35 +0530

    add factory method

---
+++ creational/factory/__init__.py
@@ -0,0 +1,8 @@
+
+# Factory Method also known as Virtual Constructor
+
+"""
+Factory method is a creational design pattern that provides an interface for creating
+objects in a superclass, but allows subclass to alter the type of objects that will be created.
+"""
+
